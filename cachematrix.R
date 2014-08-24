449e87e1696fa8cebed4403be9e9098430e82d10
brandonhu <brandon_fu@hotmail.com>
2014-08-24 12:28:39 +0800

    Update cachematrix.R
    -Add 2 functions in this file
    -1st function to configure the setter and getter methods of the matrix
    -2nd function to return the cached data copy or store the data in cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Edited by Brandon Hu 
+## Date: 24/08/2014
+## Description: Contains 2 functions
+## makeCacheMatrix: setter and getter methods of the cached matrix
+## cacheSolve: return the cached inverse matrix
 
-## Write a short comment describing this function
-
+## Set the value of the matrix
+## Get the value of the matrix 
+## Set the value of the inverse matrix (solve)
+## Get the value of the inverse matrix (solve)
 makeCacheMatrix <- function(x = matrix()) {
-
+    m_list <- NULL
+    set <- function(m_data) {
+      x <<- m_data #assign matrix data value to an obj by lexical scoping
+      m_list <<- NULL
+    }
+    get <- function() x
+    setSolve <- function(solve) m_list <<- solve
+    getSolve <- function() m_list 
+    list(set=set,get=get,setSolve=setSolve,getSolve=getSolve) #returns a list
 }
 
-
-## Write a short comment describing this function
-
+## Return the cached stored inverse matrix 
+## Else store the inversed matrix in cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m_list <- x$getSolve()
+  if (!is.null(m_list)) { #if list not null, return the inverse matrix stored in cache 
+      message("Retrieving cached data ...")
+      return(m_list) # return a matrix that is the inverse of 'x'
+  }
+  else {
+    data <- x$get()
+    m_list <-  solve(data, ...)
+    x$setSolve(m_list)
+    m_list
+    message("Store the data in cache.")
+  }
 }
